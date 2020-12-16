d5317498b32fc331f5643f2c563e59b162da2fd3
shane-liu-1010 <shaneliu1010@gmail.com>
2020-12-16 11:43:22 +0800

    update build.py

--- build.py
+++ build.py
@@ -1,42 +1,44 @@
 # -*- coding: UTF-8 -*-
 import os
 
-text="# CodeBook by 櫛風\n"
-text+="[toc]\n"
+def buildMD(Filelist):
+    text="# CodeBook by 櫛風\n"
+    text+="[toc]\n"
+    for file in Filelist:
+        try:
+            lang = file[file.find('.')+1:]
+            if lang == 'sh':
+                lang = 'shell'
+            elif lang == 'py':
+                lang = 'python'
+            elif lang == 'hpp':
+                lang = 'cpp'
+            prog = "### " + file[:file.find('.')] + "\n"
+            prog += "```" + lang + "\n"
+            f = open(home+"/"+file,'r')
+            prog += f.read()
+            f.close()
+            prog += "```\n"
+            text += prog
+        except:
+            home = file
+            text += "## "+file[2:]+"\n"
+    f = open("codebook.md",'w+')
+    f.write(text)
+    f.close()
+    print("done.")
 
-Filelist = []
-for home,dirs,files in os.walk("."):
-    files = [f for f in files if not f[0] == '.']
-    dirs[:] = [d for d in dirs if not d[0] == '.']
-    if home == ".":
-        continue
-    Filelist.append(home)
-    for filename in files:
-        Filelist.append(filename)
+def getFileist():
+    Filelist = []
+    for home,dirs,files in os.walk("."):
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        if home == ".":continue
+        Filelist.append(home)
+        for filename in files:
+            Filelist.append(filename)
+    return Filelist
 
-for file in Filelist:
-    try:
-        lang = file[file.find('.')+1:]
-        if lang == 'sh':
-            lang = 'shell'
-        elif lang == 'py':
-            lang = 'python'
-        elif lang == 'hpp':
-            lang = 'cpp'
-        prog = "### " + file[:file.find('.')] + "\n"
-        prog += "```" + lang + "\n"
-        f = open(home+"/"+file,'r')
-        print("    file> "+file)
-        prog += f.read()
-        f.close()
-        prog += "```\n"
-        text += prog
-    except:
-        home = file
-        print("dir> "+home[2:])
-        text += "## "+file[2:]+"\n"
-
-f = open("codebook.md",'w+')
-f.write(text)
-f.close()
-print("It's done.")
+Filelist = getFileist()
+buildMD(Filelist)
+# os.system("open codebook.md")

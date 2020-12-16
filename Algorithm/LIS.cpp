50cff1b933a845ab1b185b90ef1475f8138e6ce7
shane-liu-1010 <shaneliu1010@gmail.com>
2020-12-17 00:19:48 +0800

    update lis

--- Algorithm/LIS.cpp
+++ Algorithm/LIS.cpp
@@ -1,14 +1,13 @@
-int LIS(vector<double>& s){
-    if(s.empty())return 0;
-    vector<double>v;
+vector<int> LIS(vector<int>& s){
+    if(s.empty())return s;
+    vector<int>v;
     v.push_back(s[0]);
-    for (int i=1;i<s.size();i++){
+    for(int i=1;i<s.size();i++){
         double n=s[i];
-        if(n>v.back())
-            v.push_back(n);
+        if(n>v.back())v.push_back(n);
         else{
             *lower_bound(v.begin(),v.end(),n)=n;
         }
     }
-    return v.size();
+    return v;
 }

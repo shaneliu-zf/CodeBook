0e878c977042ee07bd0306f164e8d957ff18d182
shane-liu-1010 <shaneliu1010@gmail.com>
2020-12-16 11:50:59 +0800

    updat build.py

--- build.py
+++ build.py
@@ -1,33 +1,6 @@
 # -*- coding: UTF-8 -*-
 import os
 
-def buildMD(Filelist):
-    text="# CodeBook by 櫛風\n"
-    text+="[toc]\n"
-    for file in Filelist:
-        try:
-            lang = file[file.find('.')+1:]
-            if lang == 'sh':
-                lang = 'shell'
-            elif lang == 'py':
-                lang = 'python'
-            elif lang == 'hpp':
-                lang = 'cpp'
-            prog = "### " + file[:file.find('.')] + "\n"
-            prog += "```" + lang + "\n"
-            f = open(home+"/"+file,'r')
-            prog += f.read()
-            f.close()
-            prog += "```\n"
-            text += prog
-        except:
-            home = file
-            text += "## "+file[2:]+"\n"
-    f = open("codebook.md",'w+')
-    f.write(text)
-    f.close()
-    print("done.")
-
 def getFileist():
     Filelist = []
     for home,dirs,files in os.walk("."):
@@ -39,6 +12,34 @@ def getFileist():
             Filelist.append(filename)
     return Filelist
 
+def getLang(lang):
+    if lang == 'sh': lang = 'shell'
+    elif lang == 'py': lang = 'python'
+    elif lang == 'hpp': lang = 'cpp'
+    return lang
+
+def buildMD(Filelist):
+    text="# CodeBook by 櫛風\n"
+    text+="[toc]\n"
+    for file in Filelist:
+        try:
+            lang = getLang(file[file.find('.')+1:])
+            filename = file[:file.find('.')]
+            prog = "### " + filename + "\n"
+            prog += "```" + lang + "\n"
+            f = open(home+"/"+file,'r')
+            prog += f.read()
+            f.close()
+            prog += "```\n"
+            text += prog
+        except:
+            home = file
+            text += "## " + file[2:] + "\n"
+    f = open("codebook.md",'w+')
+    f.write(text)
+    f.close()
+    print("done.")
+
 Filelist = getFileist()
 buildMD(Filelist)
-# os.system("open codebook.md")
+os.system("open codebook.md")

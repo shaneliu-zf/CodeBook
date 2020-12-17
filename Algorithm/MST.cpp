c48562d7fb6f2aa1c0721227fe07d267d4bb53ba
shane-liu-1010 <shaneliu1010@gmail.com>
2020-12-17 12:18:01 +0800

    update MST.cpp

--- Algorithm/MST.cpp
+++ Algorithm/MST.cpp
@@ -1,26 +1,18 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
-struct edge{
-	int from;
-	int to;
-	int w;
-};
-
+struct edge{int from,to,w;};
 int n, m, ans;
 int p[100005];
 edge e[100005];
-
 int Find(int x){
 	if(p[x]!=x)p[x]=Find(p[x]);
 	return p[x];
 }
-
 bool Same(int x, int y){return Find(x)==Find(y);}
 void Union(int x, int y){p[Find(x)]=Find(y);}
 bool cmp(edge a,edge b){return a.w<b.w;}
-int main() {
+int main(){
 	cin>>n>>m;
 	for(int i=0;i<=n;i++)p[i]=i;
 	for(int i=0;i<m;i++)cin>>e[i].from>>e[i].to>>e[i].w;

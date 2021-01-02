de188b806b52550f4fdfebe8def851b1927602e5
shane-liu-1010 <shaneliu1010@gmail.com>
2021-01-02 13:43:39 +0800

    update KMP

--- Algorithm/KMP.cpp
+++ Algorithm/KMP.cpp
@@ -1,5 +1,5 @@
 int KMP(string pat,string txt){
-    vector<vector<int>>dp(pat.length(), vector<int>(256,0));
+    vector<vector<int>>dp(pat.length(),vector<int>(256,0));
     dp[0][pat[0]]=1;
     int x=0;
     for(int j=1;j<pat.length();j++){

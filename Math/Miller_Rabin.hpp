20817c7ec2923033be41293e2336b3de963c3744
shaneliu <shaneliu1010@gmail.com>
2021-05-29 21:52:34 +0800

    修改miller_rabin跟快速冪

--- Math/Miller_Rabin.hpp
+++ Math/Miller_Rabin.hpp
@@ -1,5 +1,5 @@
 //need: fastMulti、fastPower
-bool millerRabin(num n,int times=20){
+/*bool millerRabin(num n,int times=20){
 	if(n==2)return true;
 	if(n<2||n%2==0)return false;
 	for(int i=1;i<=times;){
@@ -17,5 +17,25 @@ bool millerRabin(num n,int times=20){
         }
         next:i++;
 	}
+	return true;
+}*/
+bool miillerRabin(int n,int k=20){
+	if(n<=1||n==4)return false;
+	if(n<=3)return true;
+	int d=n-1;
+	while(d%2==0)d/=2;
+	for(int i=0;i<k;i++)
+		if(![](int d, int n){
+        	int a=2+rand()%(n-4);
+        	int x=fastPower(a,d,n);
+        	if (x==1||x==n-1)return true;
+        	while (d!=n-1){
+        		x=(x*x)%n;
+        		d*=2;
+        		if(x==1)return false;
+        		if(x==n-1)return true;
+        	}
+        	return false;
+        }(d,n))return false;
 	return true;
 }
--- Math/fast_power.hpp
+++ Math/fast_power.hpp
@@ -1,3 +1,17 @@
+
+inline int fastPower(int x,unsigned int y,int p){
+	int res = 1;
+	x = x % p;
+	while (y > 0){
+        if (y & 1)res = (res*x) % p;
+		y = y>>1;
+		x = (x*x) % p;
+	}
+	return res;
+}
+
+/* 好像有問題
+
 inline num fastPower(num a,num b,num mod=0){
 	num ans=1;
 	while(b){
@@ -6,4 +20,4 @@ inline num fastPower(num a,num b,num mod=0){
 		b>>=1;
 	}
 	return ans;
-}
+}*/
